aa8b5446c6f6c714445f468fb79fe6866d097fc2
Hye-m-in <kus09127@naver.com>
2024-06-15 02:20:11 +0900

    게임시작 페이지에 뒤로가기버튼

--- ComputerGraphics/Assets/Script/Initialize.cs
+++ ComputerGraphics/Assets/Script/Initialize.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class ButtonController : MonoBehaviour
+public class Initialize : MonoBehaviour
 {
-    Image button;
-
     // Start is called before the first frame update
     void Start()
     {
-        button = GetComponent<Image>();
-        button.alphaHitTestMinimumThreshold = 0.1f; //붙투명한 부분만 클릭 인식
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
     }
 
     // Update is called once per frame
