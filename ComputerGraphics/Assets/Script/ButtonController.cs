76409b1fa9179da593e6135894faafbcab2e1a48
Hye-m-in <kus09127@naver.com>
2024-06-15 13:52:46 +0900

    Revert "게임시작 페이지에 뒤로가기버튼"
    This reverts commit aa8b5446c6f6c714445f468fb79fe6866d097fc2.

--- ComputerGraphics/Assets/Script/ButtonController.cs
+++ ComputerGraphics/Assets/Script/ButtonController.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
-    Image button;
+    Image passport;
 
     // Start is called before the first frame update
     void Start()
     {
-        button = GetComponent<Image>();
-        button.alphaHitTestMinimumThreshold = 0.1f; //붙투명한 부분만 클릭 인식
+        passport = GetComponent<Image>();
+        passport.alphaHitTestMinimumThreshold = 0.1f; //붙투명한 부분만 클릭 인식
     }
 
     // Update is called once per frame
